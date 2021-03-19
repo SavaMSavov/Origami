22d6f3c12ae69c4d9f6d856c1a5ff99f893b8816
Sava Savov <savamiroslavovsavov@gmail.com>
2021-03-19 10:03:20 +0200

    edit navigationItem

--- client/src/Components/Header/NavigationItem/NavigationItem.js
+++ client/src/Components/Header/NavigationItem/NavigationItem.js
@@ -2,7 +2,7 @@ import style from "./NavigationItem.module.css";
 
 const NavigationItem = (props) => {
   return (
-    <li className="listItem">
+    <li className={style.listItem}>
       <a href="#">{props.children}</a>
     </li>
   );
